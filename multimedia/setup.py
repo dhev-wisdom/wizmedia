119120f3e22a50e925da179c397586a10d2be857
dhev-wisdom <nonsowisdom62@gmail.com>
2023-08-27 20:28:40 +0100

    Deployment fix

---
+++ multimedia/setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(name='django',
+      version='4.2.4',
+      description='Django Multimedia App',
+      author='Dev-Wisdom',
+      author_email='nonsowisdom62@gmail.com',
+      url='https://github.com/dhev-wisdom/wizmedia/',
+      packages=['asgiref', 'certifi', 'charset-normalizer', 'Django',
+          'gunicorn', 'idna', 'packaging', 'Pillow', 'python-dotenv',
+          'requests', 'sqlparse','typing_extensions','urllib3']
+     )
