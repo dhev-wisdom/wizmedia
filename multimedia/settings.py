2108aac59a3914b835816c7126c277c9e67dcb6c
dhev-wisdom <nonsowisdom62@gmail.com>
2023-08-27 20:48:45 +0100

    allowed host

--- multimedia/settings.py
+++ multimedia/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-yybthd)zgwzo0qm2g8ege*c9p)*y3t)*+*zg^qa8n2+w_)5r^d
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['wizmedia-fa1a7ca267da.herokuapp.com', 'localhost']
 
 
 # Application definition
